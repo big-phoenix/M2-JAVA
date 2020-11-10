7d0b3f94b96c4d7eea127afa2e0ae17a6526c876
Javi <bisistems@gmail.com>
2020-11-10 19:04:01 +0100

    Fase 1

---
+++ lletres_repetides/Lletres_Repetides.java
@@ -0,0 +1,32 @@
+package lletres_repetides;
+
+public class Lletres_Repetides {
+
+	public static void main(String[] args) {
+		
+		char[] nom = new char[10];
+		
+		nom[0] = 'j';
+		nom[1] = 'o';
+		nom[2] = 's';
+		nom[3] = 'e';
+		nom[4] = 'j';
+		nom[5] = 'a';
+		nom[6] = 'v';
+		nom[7] = 'i';
+		nom[8] = 'e';
+		nom[9] = 'r';
+		
+		
+		for(int i=0; i< nom.length; i++) {
+			
+			System.out.print(nom[i]);
+			
+		}
+	
+
+	}
+	
+	
+
+}
